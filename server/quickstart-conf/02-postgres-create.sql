3abad1c5a27160f7ea91f0cbf465b9253a66e14c
niuyh <you@example.com>
2018-02-25 20:09:11 +0800

    model

--- server/quickstart-conf/02-postgres-create.sql
+++ server/quickstart-conf/02-postgres-create.sql
@@ -4,51 +4,63 @@
 
 DROP TABLE IF EXISTS USE_CASE_3, USE_CASE_2, USE_CASE_1, PROJECT, BOOK, AUTHOR, USER_ROLE, ROLE, PASSPORT, USER_;
 
-CREATE SEQUENCE user_id increment by 1 minvalue 1 no maxvalue start with 1;
+CREATE SEQUENCE user_id
+  INCREMENT BY 1
+  MINVALUE 1
+  NO MAXVALUE
+  START WITH 1;
 CREATE TABLE USER_ (
-    id                   int   primary key  default nextval('user_id'),
-    login                    varchar(100) not null ,
-    password                 varchar(100) not null,
-    email                    varchar(100),
-    is_enabled               bool not null default true,
-    civility                 char(2) default 'KM',
-    country_code             varchar(6) default '+33',
-    first_name               varchar(100),
-    last_name                varchar(100),
--- audit (detected by celerio by convention)
-    creation_date            timestamp DEFAULT NOW(),
-    creation_author          varchar(200),
-    last_modification_date   timestamp default NOW(),
-    last_modification_author varchar(200),
--- optimistic lock (detected by celerio by convention)
-    version                  int default 0
---     constraint user_unique_1 unique (login)
-) ;
+  id                       INT PRIMARY KEY       DEFAULT nextval('user_id'),
+  login                    VARCHAR(100) NOT NULL,
+  password                 VARCHAR(100) NOT NULL,
+  email                    VARCHAR(100),
+  is_enabled               BOOL         NOT NULL DEFAULT TRUE,
+  civility                 CHAR(2)               DEFAULT 'KM',
+  country_code             VARCHAR(6)            DEFAULT '+33',
+  first_name               VARCHAR(100),
+  last_name                VARCHAR(100),
+  -- audit (detected by celerio by convention)
+  creation_date            TIMESTAMP             DEFAULT NOW(),
+  creation_author          VARCHAR(200),
+  last_modification_date   TIMESTAMP             DEFAULT NOW(),
+  last_modification_author VARCHAR(200),
+  -- optimistic lock (detected by celerio by convention)
+  version                  INT                   DEFAULT 0
+  --     constraint user_unique_1 unique (login)
+);
 
-CREATE SEQUENCE PASSPORT_id increment by 1 minvalue 1 no maxvalue start with 1;
+CREATE SEQUENCE PASSPORT_id
+  INCREMENT BY 1
+  MINVALUE 1
+  NO MAXVALUE
+  START WITH 1;
 
 CREATE TABLE PASSPORT (
-    id                       int primary key  default nextval('PASSPORT_id'),
-    passport_number          varchar(100) not null,
-    expiration_date          date,
-    holder_id                int unique not null  references USER_(id)
-) ;
-CREATE SEQUENCE ROLE_id increment by 1 minvalue 1 no maxvalue start with 1;
+  id              INT PRIMARY KEY  DEFAULT nextval('PASSPORT_id'),
+  passport_number VARCHAR(100) NOT NULL,
+  expiration_date DATE,
+  holder_id       INT UNIQUE   NOT NULL  REFERENCES USER_ (id)
+);
+CREATE SEQUENCE ROLE_id
+  INCREMENT BY 1
+  MINVALUE 1
+  NO MAXVALUE
+  START WITH 1;
 
 CREATE TABLE ROLE (
-    id              int primary key  default nextval('ROLE_id'),
-    role_name       varchar(100) not null UNIQUE
+  id        INT PRIMARY KEY  DEFAULT nextval('ROLE_id'),
+  role_name VARCHAR(100) NOT NULL UNIQUE
 );
 
 CREATE TABLE USER_ROLE (
-    user_id     int not null references USER_(id),
-    role_id     int not null references ROLE(id),
-    primary key (user_id, role_id)
+  user_id INT NOT NULL REFERENCES USER_ (id),
+  role_id INT NOT NULL REFERENCES ROLE (id),
+  PRIMARY KEY (user_id, role_id)
 );
 
 
-
-INSERT INTO USER_ (id, login, password, email, is_enabled, version) VALUES (-1, 'admin', 'admin', 'admin@example.com', true, 1);
+INSERT INTO USER_ (id, login, password, email, is_enabled, version)
+VALUES (-1, 'admin', 'admin', 'admin@example.com', TRUE, 1);
 
 INSERT INTO ROLE (id, role_name) VALUES (-1, 'ROLE_ADMIN');
 INSERT INTO ROLE (id, role_name) VALUES (-2, 'ROLE_USER');
@@ -57,4 +69,52 @@ INSERT INTO ROLE (id, role_name) VALUES (-3, 'ROLE_MONITORING');
 INSERT INTO USER_ROLE (user_id, role_id) VALUES (-1, -1);
 INSERT INTO USER_ROLE (user_id, role_id) VALUES (-1, -2);
 INSERT INTO USER_ROLE (user_id, role_id) VALUES (-1, -3);
+
+-- APP_
+CREATE SEQUENCE APP_ID
+  INCREMENT BY 1
+  MINVALUE 1
+  NO MAXVALUE
+  START WITH 1;
+CREATE TABLE APP_ (
+  id            INT PRIMARY KEY  DEFAULT nextval('APP_ID'),
+  category_     VARCHAR(50),
+  template_path VARCHAR(500)
+);
+CREATE SEQUENCE APP_WIDGET_ID
+  INCREMENT BY 1
+  MINVALUE 1
+  NO MAXVALUE
+  START WITH 1;
+CREATE TABLE APP_WIDGET (
+  id             INT PRIMARY KEY  DEFAULT nextval('APP_WIDGET_ID'),
+  APP_ID         INT NOT NULL REFERENCES APP_ (id),
+  RULE           VARCHAR(4000),
+  user_interface VARCHAR(4000)
+);
+-- CONFIG_
+CREATE SEQUENCE CONFIG_ID
+  INCREMENT BY 1
+  MINVALUE 1
+  NO MAXVALUE
+  START WITH 1;
+CREATE TABLE CONFIG_ (
+  id         INT PRIMARY KEY  DEFAULT nextval('CONFIG_ID'),
+  APP_ID     INT NOT NULL REFERENCES APP_ (id),
+  user_id    INT REFERENCES USER_ (id),
+  RULE_VALUE VARCHAR(4000)
+);
+--  ARTIFACT
+CREATE SEQUENCE ARTIFACT_ID
+  INCREMENT BY 1
+  MINVALUE 1
+  NO MAXVALUE
+  START WITH 1;
+CREATE TABLE ARTIFACT (
+  id          INT PRIMARY KEY  DEFAULT nextval('ARTIFACT_ID'),
+  APP_ID      INT NOT NULL REFERENCES APP_ (id),
+  CONFIG_ID   INT NOT NULL REFERENCES CONFIG_ (id),
+  output_path VARCHAR(500)
+);
+
 

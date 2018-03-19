8fc9b53502ce15e0ea198a08ba88370be978d8dd
root <root@LAPTOP-V3EVQ2DD.localdomain>
2018-03-19 12:47:17 +0800

    r

--- frontend/src/main/java/com/willbe/giftapp/WebViewActivity.java
+++ frontend/src/main/java/com/willbe/giftapp/WebViewActivity.java
@@ -29,8 +29,6 @@ public class WebViewActivity extends AppCompatActivity {
             }
         });
 
-        webView.loadUrl("http://10.0.2.2:4200");
-//        webView.loadUrl("http://localhost:4200/");
-//        webView.loadUrl("http://app.gift.com:4200/");
+        webView.loadUrl("http://10.0.2.2:4200/");
     }
 }
